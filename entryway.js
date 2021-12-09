f96836662406102db723ad9b4c3fb9e3fd815497
brigid <48567955+bnemeton@users.noreply.github.com>
2021-12-09 15:21:57 -0800

    fixroutes

--- entryway.js
+++ entryway.js
@@ -3,17 +3,19 @@ var urlParams = new URLSearchParams(window.location.search)
 let choice = urlParams.get("arrival") || "error"
 
 let p = document.getElementById('text')
-
 p.innerText = `${choice}`
 
 switch (choice) {
     case "horse":
         document.getElementById("horse").removeAttribute("hidden")
+        p.innerText = "on horseback"
         break;
     case "walk":
         document.getElementById("walk").removeAttribute("hidden")
+        p.innerText = `on foot`
          break;
     case "pursuit":
         document.getElementById("pursuit").removeAttribute("hidden")
+        p.innerText = `pursued by mysterious foes`
         break;
 }
