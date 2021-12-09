2ac7930f9b93363b5b03349e43214b9f46037de9
brigid <48567955+bnemeton@users.noreply.github.com>
2021-12-09 14:08:07 -0800

    fixoption

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /// var arrivalForm = Document.getElementByID("arrival-form");
 var options = Document.getElementByID("arrival-select").options;
-var choice = options[options.selectedIndex].id;
+
 
 function handleSubmit(e) {
     e.preventDefault();
+    var choice = options[options.selectedIndex].id;
     window.location.href(`entryway.html/?arrival=${choice}`)
 }
