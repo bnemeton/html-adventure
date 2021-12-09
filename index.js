785153fba52d11c22588149d813cf22033ee0706
brigid <48567955+bnemeton@users.noreply.github.com>
2021-12-09 11:29:48 -0800

    form submit

---
+++ index.js
@@ -0,0 +1,7 @@
+/// var arrivalForm = Document.getElementByID("arrival-form");
+var choice = Document.getElementByID("arrival-select").value;
+
+function handleSubmit(e) {
+    e.preventDefault();
+    window.location.replace(`entryway/?arrival=${choice}`)
+}
