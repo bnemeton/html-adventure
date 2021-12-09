01d05b3905944b7a43b89b9b6f7c4e0771359d10
brigid <48567955+bnemeton@users.noreply.github.com>
2021-12-09 15:23:28 -0800

    jhmgvjhg

--- entryway.js
+++ entryway.js
@@ -3,19 +3,18 @@ var urlParams = new URLSearchParams(window.location.search)
 let choice = urlParams.get("arrival") || "error"
 
 let p = document.getElementById('text')
-p.innerText = `${choice}`
 
 switch (choice) {
     case "horse":
         document.getElementById("horse").removeAttribute("hidden")
-        p.innerText = "on horseback"
+        p.innerText = " on horseback"
         break;
     case "walk":
         document.getElementById("walk").removeAttribute("hidden")
-        p.innerText = `on foot`
+        p.innerText = ` on foot`
          break;
     case "pursuit":
         document.getElementById("pursuit").removeAttribute("hidden")
-        p.innerText = `pursued by mysterious foes`
+        p.innerText = `, pursued by mysterious foes`
         break;
 }
