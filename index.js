05038ac98ff9c7dbc6cda7efb4098d4ba23c927d
brigid <48567955+bnemeton@users.noreply.github.com>
2021-12-09 14:09:49 -0800

    chooseArrival

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 var options = Document.getElementByID("arrival-select").options;
 
 
-function handleSubmit(e) {
-    e.preventDefault();
+function chooseArrival() {
+   /// e.preventDefault();
     var choice = options[options.selectedIndex].id;
     window.location.href(`entryway.html/?arrival=${choice}`)
 }
