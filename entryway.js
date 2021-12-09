55f47d17096e8477395f6f4e65cc373cadaff27c
brigid <48567955+bnemeton@users.noreply.github.com>
2021-12-09 15:11:20 -0800

    hidden horse

--- entryway.js
+++ entryway.js
@@ -5,3 +5,9 @@ let choice = urlParams.get("arrival") || "error"
 let p = document.getElementById('text')
 
 p.innerText = `${choice}`
+
+switch (choice) {
+    case "horse":
+        document.getElementById("horse").removeAttribute("hidden")
+
+}
