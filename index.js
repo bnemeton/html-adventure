4660b3d0a548fbdb7c4ea505592d2cd05f9b12b8
brigid <48567955+bnemeton@users.noreply.github.com>
2021-12-09 14:12:11 -0800

    fire

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /// var arrivalForm = Document.getElementByID("arrival-form");
-var options = Document.getElementByID("arrival-select").options;
+
 
 
 function chooseArrival() {
    /// e.preventDefault();
+    var options = Document.getElementByID("arrival-select").options;
     var choice = options[options.selectedIndex].id;
     window.location.href(`entryway.html/?arrival=${choice}`)
 }
